d8bbe391e566396ede914cb01679fd76d5014a95
marco-soria <marco.soria@pucp.edu.pe>
2024-02-22 17:54:45 -0500

    loopstudios

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+function mostrar_menu(){
+    document.getElementById("mostrar-menu").classList.toggle('mostrar-v');
+}
